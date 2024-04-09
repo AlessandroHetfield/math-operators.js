dd1afa2d7ed8683f9f7158297cd050c835df5a38
AlessandroHetfield <160076296+AlessandroHetfield@users.noreply.github.com>
2024-04-09 18:42:04 -0400

    Add files via upload

---
+++ 2. Mathoperators.js
@@ -0,0 +1,14 @@
+var job = ('programmer');
+//Math Operators
+
+const now= 2020;
+const ageJonas = now-1991;
+const ageSarah = now - 2018;
+console.log ('Jonas is', ageJonas/2);
+console.log ('Sara is', ageSarah*2);
+
+console.log (ageJonas/ 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of = 2*2*2
+const firstName= "Jonas";
+const lastName= 'Schemdtman';
+console.log(firstName + ' '+ lastName)
